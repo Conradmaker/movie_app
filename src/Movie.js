d5705b79e41801b2b19dffce1c529372bc7ea17f
yhg0337 <58197935+yhg0337@users.noreply.github.com>
2020-06-26 14:47:49 +0900

    완성!!

--- src/Movie.js
+++ src/Movie.js
@@ -1,30 +1,31 @@
 import React from "react";
-import PropsTypes from "prop-types";
-
-function Movie({ year, title, summary, poster, genre }) {
+import PropTypes from "prop-types";
+import "./Movie.css";
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="genres">
-          {genre.map((genre, index) => (
-            <li key="index" className="genres_genre">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
 }
-
-Movie.PropsTypes = {
-  id: PropsTypes.number.isRequired,
-  year: PropsTypes.number.isRequired,
-  title: PropsTypes.string.isRequired,
-  summary: PropsTypes.string.isRequired,
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 export default Movie;
